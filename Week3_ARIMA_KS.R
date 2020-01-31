6ce9c4df7da3f54d6424c099d52e1377de8959fe
Kristy Sullivan <ksull027@fiu.edu>
2020-01-31 13:58:51 -0500

    ARIMA 1.31.2020

--- Week3_ARIMA_KS.R
+++ Week3_ARIMA_KS.R
@@ -15,7 +15,7 @@ plot(nee, typ="l", ylab= "NEE", xlab="")
 
 #Remove outliers w/ tsclean()
 plot(nee)
-lines(tsclean(nee), col="red")
+lines(tsclean(nee), col="blue")
 nee <- tsclean(nee)
 
 #Decomposition of time series (observed, trend, seasonal, & random)
@@ -97,70 +97,6 @@ arima.nee3 <-auto.arima(nee, xreg=c(diff(sal),0), trace=TRUE)
 #AIC score indicates which model is better, lower AIC = better model
 AIC(arima.nee2, arima.nee3 )
 
-#Adding salinity to NEE did not improve the model
-    #--> Check if extreme salinity is more important
-    #Create salinity index
-sal.i <- sal
-sal.i[sal.i < 25 ]<- 0
-sal.i[sal.i >= 25 ]<- 1
-
-plot(sal.i)
-
-#Try adding extreme salinity indicator into the model & see if it improves
-arima.nee4 <-auto.arima(nee, xreg=sal.i, trace=TRUE)
-
-#Compare to current model
-#AIC score indicates which model is better, higher AIC = better
-AIC(arima.nee2,arima.nee4 )
-
-#Test for independence
-#Measuring for significant difference from white noise
-#Need p-value greater than 0.05
-checkresiduals(arima.nee4, lag=36)
-
-par(mfrow=c(1,1))
-plot(nee , typ="l"); lines(fitted(arima.nee4),col="red")
-
-################# CAN YOU MAKE A BETTER MODEL? #################
-
-#Try using PAR:
-
-
-#Create timeseries object:
-parlight<-ts(mangroves$par, start=1, frequency=30)
-
-#Visualize data:
-par(mfrow=c(1,1), mai=c(1.25,0.8,0.1, 0.1))
-plot(parlight, typ="l", ylab= "PAR", xlab="")
-
-#Remove outliers
-plot(parlight , typ="l", ylab= "PAR", xlab="")
-lines(tsclean(parlight) , col="red")
-
-parlight <- tsclean(parlight)
-
-#Decompose time series
-parlight.d <- decompose(parlight, 'multiplicative')
-plot(parlight.d)
-
-#Test for stationarity:
-#P-value < 0.05 indicates the time series is stationary
-adf.test(parlight)
-#If null hypothesis is not rejected, try differencing the time series w/ diff()
-adf.test(diff(parlight))
-
-#Look for significant lags:
-ccf( diff(parlight),nee, na.action = na.pass, lag.max=40, plot=TRUE)
-
-#Explore models of NEE:
-#auto.arima() helps generate optimal p, d, & q
-arima.nee5 <-auto.arima(nee, xreg=c(diff(parlight),0), trace=TRUE)
-
-#Compare to current model
-#AIC score indicates which model is better, lower AIC = better model
-AIC(arima.nee4, arima.nee5 )
-
-
 #________________________________________________________________
 #Adding salinity to NEE did not improve the model
 #--> Check if extreme salinity is more important
@@ -185,6 +121,69 @@ checkresiduals(arima.nee4, lag=36)
 
 par(mfrow=c(1,1))
 plot(nee , typ="l"); lines(fitted(arima.nee4),col="red")
+
+
+################# CAN YOU MAKE A BETTER MODEL? #################
+
+#Try using PAR:
+
+#Create timeseries object:
+parlight<-ts(mangroves$par, start=1, frequency=30)
+
+#Visualize data:
+par(mfrow=c(1,1), mai=c(1.25,0.8,0.1, 0.1))
+plot(parlight, typ="l", ylab= "PAR", xlab="")
+
+#Remove outliers
+plot(parlight , typ="l", ylab= "PAR", xlab="")
+lines(tsclean(parlight) , col="red")
+
+parlight <- tsclean(parlight)
+
+#Decompose time series
+parlight.d <- decompose(parlight, 'multiplicative')
+plot(parlight.d)
+
+#Test for stationarity:
+#P-value < 0.05 indicates the time series is stationary
+adf.test(parlight)
+#If null hypothesis is not rejected, try differencing the time series w/ diff()
+adf.test(diff(parlight))
+
+#Detecting autocorrelation:
+#Auto correlation function (ACF) plots display correlation between a series & its lags
+#Series correlated with lags indicates seasonal trends/components
+acf(parlight, lag.max=45)
+pacf(parlight, lag.max=45)
+
+#Look for significant lags:
+ccf( diff(parlight),nee, na.action = na.pass, lag.max=40, plot=TRUE)
+
+#Explore models of NEE:
+#auto.arima() helps generate optimal p, d, & q
+arima.nee5 <-auto.arima(nee, xreg=c(diff(parlight),0), trace=TRUE)
+
+#Test out the auto-generated ARIMA model values:
+#Residuals should be normally distributed
+tsdisplay(residuals(arima.nee5), lag.max=45)
+
+#Compare to current model
+#AIC score indicates which model is better, lower AIC = better model
+AIC(arima.nee4, arima.nee5 )
+par(mfrow=c(1,1))
+plot(nee , typ="l"); lines(fitted(arima.nee5),col="red")
+
+
+#Test for independence
+#Measuring for significant difference from white noise
+#Need p-value greater than 0.05
+checkresiduals(arima.nee5, lag=36)
+
+par(mfrow=c(1,1))
+plot(nee , typ="l", col="blue"); lines(fitted(arima.nee5),col="red")
+
+#Forecast NEE for 30 days:
+plot(forecast(arima.nee5, h=30))
 
 
 
